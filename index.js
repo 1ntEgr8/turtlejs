046e7177d9f52fb6b2731a9dafcf6ddd799c8ce4
Elton <eltonp3103@gmail.com>
2020-01-01 23:29:08 -0500

    mvp done

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const turtle = new Turtle(200, 200, 500, 500);
-turtle.forward(300);
-turtle.rotate(-120);
+
+for (let i = 0; i < 50; i++) {
+    turtle.forward(200);
+    turtle.rotate(-123);
+}
+
+turtle.done();
--- turtle.js
+++ turtle.js
@@ -1,83 +1,147 @@
 let nx, ny, dx, dy, duration, startTime; // variables used in animating turtle
+/*
+    When the user calls forward, rotate, backward, ...
+        push the operation to the list
+    When invokes done
+        start executing the operations
+
+    Action object => {
+        action: 'r', // single character code
+        values: [], // array of values passed to dispatch function
+    }
+*/
+
+function degToRad(degrees) {
+    return (degrees * Math.PI) / 180;
+}
 
 class Turtle {
-    constructor(x=0, y=0, windowWidth=100, windowHeight=100) {
-       console.log("%cTurtle born!", "color: green; font-size: 2rem; font-weight: bold;");
+    constructor(x = 0, y = 0, windowWidth = 100, windowHeight = 100) {
+        console.log(
+            "%cTurtle born!",
+            "color: green; font-size: 2rem; font-weight: bold;"
+        );
 
-       this.x = x;
-       this.y = y;
-       this.windowWidth = windowWidth;
-       this.windowHeight = windowHeight;
-       this.heading = 0;
-       this.path = `M ${x} ${y}`;
-       this.speed = 0.2;
-       this.isAnim = true;
+        this.init_x = x;
+        this.init_y = y;
+        this.x = x;
+        this.y = y;
+        this.windowWidth = windowWidth;
+        this.windowHeight = windowHeight;
+        this.heading = 0;
+        this.isAnim = true;
 
-       // creating the canvas on which the turtle will move 
-       this.canvas = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-       this.canvas.setAttributeNS(null, 'width', `${this.windowWidth}px`);
-       this.canvas.setAttributeNS(null, 'height', `${this.windowHeight}px`);
+        this.speed = 0.2;
 
-       // setting up the turtle
-       // TODO: add ability to configure turtle
-       this.turtle = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-       this.turtle.setAttributeNS(null, 'd', this.path);
-       this.turtle.setAttributeNS(null, 'stroke', 'black');
-       this.turtle.setAttributeNS(null, 'stroke-width', '5px');
-       this.turtle.setAttributeNS(null, 'fill', 'none');
+        this.dParts = [
+            {
+                type: "M",
+                values: [x, y]
+            }
+        ];
+        this.path = "";
 
-       // appending to DOM
-       this.canvas.appendChild(this.turtle);
-       document.body.appendChild(this.canvas);
+        // creating the canvas on which the turtle will move
+        this.canvas = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        this.canvas.setAttributeNS(null, "width", `${this.windowWidth}px`);
+        this.canvas.setAttributeNS(null, "height", `${this.windowHeight}px`);
+
+        // setting up the turtle
+        // TODO: add ability to configure turtle
+        this.turtle = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "path"
+        );
+        this.turtle.setAttributeNS(null, "stroke", "black");
+        this.turtle.setAttributeNS(null, "stroke-width", "1px");
+        this.turtle.setAttributeNS(null, "fill", "none");
+
+        // appending to DOM
+        this.canvas.appendChild(this.turtle);
+        document.body.appendChild(this.canvas);
+
+        // bookkeeping for animation
+        this.currentProcessIndex = 0;
     }
 
-    animate() {
-        let time = Date.now();
-        this.x += dx;
-        this.y += dy;
-        const path = `${this.turtle.getAttributeNS(null, 'd')} L ${this.x} ${this.y}`;
-        this.turtle.setAttributeNS(null, 'd', path); 
-        if (time - startTime > duration) {
-            this.x = this.x2;
-            this.y = this.y2;
-            this.turtle.setAttributeNS(null, 'd', this.path); 
-        } else {
-            requestAnimationFrame(this.animate.bind(this));
-        }
-    }
-
-    /*
     forward(distance) {
-        this.x2 = this.x + distance * Math.cos(this.heading);
-        this.y2 = this.y + distance * Math.sin(this.heading);
-
-        this.path = `${this.turtle.getAttributeNS(null, 'd')} L ${this.x2} ${this.y2}`;
-
-
-        duration = Math.sqrt(Math.pow(this.x2 - this.x, 2), Math.pow(this.y2 - this.y, 2)) / this.speed;
-        dx = (this.x2 - this.x) * 16 / duration;
-        dy = (this.y2 - this.y) * 16 / duration;
-        
-        if (this.isAnim) {
-            startTime = Date.now();
-            this.animate();
-        } else {
-            this.x = this.toX;
-            this.y = this.toY;
-            this.turtle.setAttributeNS(null, 'd', this.path);
-        }
+        this.x = this.x + distance * Math.cos(this.heading);
+        this.y = this.y + distance * Math.sin(this.heading);
+        this.dParts.push({
+            type: "L",
+            values: [this.x, this.y]
+        });
     }
-    */
 
     rotate(degrees) {
-        this.heading += this.degToRad(degrees);
-    }
-
-    degToRad(degrees) {
-        return degrees * Math.PI / 180;
+        this.heading += degToRad(degrees);
     }
 
     done() {
-        this.animate();
+        this._dispatch();
+    }
+
+    _dispatch() {
+        this.x = this.init_x;
+        this.y = this.init_y;
+        if (this.dParts.length > 0) {
+            this._animate(this.dParts[0]);
+        }
+    }
+
+    _animate(el) {
+        switch (el.type) {
+            case "M": {
+                this.path += ` M ${el.values[0]} ${el.values[1]}`;
+                this.turtle.setAttributeNS(
+                    null,
+                    "d",
+                    this.path
+                );
+                this._animateNext();
+                break;
+            }
+            case "L": {
+                nx = el.values[0];
+                ny = el.values[1];
+                duration =
+                    Math.sqrt(Math.pow(nx - this.x, 2) + Math.pow(ny - this.y, 2)) / this.speed;
+                console.log(duration);
+                dx = ((nx - this.x) * 16) / duration;
+                dy = ((ny - this.y) * 16) / duration;
+                startTime = Date.now();
+
+                this._linearAnimate();
+                break;
+            }
+            default:
+                console.error("Invalid command, or not supported yet");
+        }
+    }
+
+    _linearAnimate() {
+        const time = Date.now();
+        this.x += dx;
+        this.y += dy;
+        if (time - startTime > duration) {
+            this.x = nx;
+            this.y = ny;
+            this.path += ` L ${this.x} ${this.y}`; 
+            this.turtle.setAttributeNS(null, "d", this.path);
+            this._animateNext();
+        } else {
+            const path = `${this.turtle.getAttributeNS(null, "d")} L ${this.x} ${this.y}`;
+            this.turtle.setAttributeNS(null, "d", path);
+            requestAnimationFrame(this._linearAnimate.bind(this));
+        }
+    }
+
+    _animateNext() {
+        this.currentProcessIndex++;
+        if (this.currentProcessIndex >= this.dParts.length) {
+            // stop animating
+        } else {
+            this._animate(this.dParts[this.currentProcessIndex]);
+        }
     }
 }
